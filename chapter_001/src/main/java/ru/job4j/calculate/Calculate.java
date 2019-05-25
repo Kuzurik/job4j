e618633682fc44561fa2a9a2d087bdb38b0f567e
kuzurik <vorota-24@bk.ru>
2019-05-25 20:14:30 +0300

    1.3 JavaDoc code convention[#143021]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
-
+    /**
+    * Calculate.
+    * @author Aliaksandr Kuzura.
+    * @since 25.05.2019.
+    * @version 1.  
+    */
 
 public class Calculate{
+    /**
+    * Main.
+    * @param arg.
+    */
     
     public static void main(String[] args){
         System.out.println("HelloWorld");
