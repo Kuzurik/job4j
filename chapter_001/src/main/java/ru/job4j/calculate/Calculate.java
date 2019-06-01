629f93a87fbbf06eaba8daa27be7ab97df942050
kuzurik <vorota-24@bk.ru>
2019-06-01 15:03:24 +0300

    "2.5. Инспектор кода IDEA. [142972]"

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,30 +1,30 @@
 package ru.job4j.calculate;
+/**
+ * Calculate.
+ * @author Aliaksandr Kuzura.
+ * @since 25.05.2019.
+ * @version 1.
+ */
+
+public class Calculate {
+
+
+
+
     /**
-    * Calculate.
-    * @author Aliaksandr Kuzura.
-    * @since 25.05.2019.
-    * @version 1.  
-    */
+     * Main.
+     * @param args
+     */
 
-	public class Calculate {
-
-    
-
-
-    /**
-    * Main.
-    * @param arg.
-    */
-    
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-}
+    }
 
     /**
-    * Method echo.
-    * @param name My name.
-    * @return Echo plus my name.
-    */
+     * Method echo.
+     * @param name My name.
+     * @return Echo plus my name.
+     */
 
     public String echo(String name) {
 
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -4,26 +4,26 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-    /**
-    * Test.
-    *
-    * @author Aliaksandr Kuzura (vorota-24@bk.ru)
-    * @version $Id$
-    * @since 0.1
-    */
+/**
+ * Test.
+ *
+ * @author Aliaksandr Kuzura (vorota-24@bk.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 
-    public class CalculateTest {
+public class CalculateTest {
     /**
-    * Test echo.
-    */
+     * Test echo.
+     */
     @Test
-        public void whenTakeNameThenThreeEchoPlusName() {
+    public void whenTakeNameThenThreeEchoPlusName() {
         String input = "Aliaksander Kuzura";
-        String expect = "Echo, echo, echo : Aliaksander Kuzura"; 
+        String expect = "Echo, echo, echo : Aliaksander Kuzura";
         Calculate calc = new Calculate();
         String result = calc.echo(input);
         assertThat(result, is(expect));
- }
-
-
     }
+
+
+}
