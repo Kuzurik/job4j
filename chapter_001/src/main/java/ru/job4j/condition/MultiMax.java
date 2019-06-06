4098906c29a490a103e699fd9d9d3f15de75b62c
kuzurik <vorota-24@bk.ru>
2019-06-07 01:17:39 +0300

    "4.4. Максимум из трех чисел [#142981]"

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,38 @@
+package ru.job4j.condition;
+
+/**
+ *  MultiMax number
+ *
+ * @author Aliaksandr Kuzura (vorota-24@bk.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class MultiMax {
+
+    /**
+     * comparison of three numbers
+     * @param first number
+     * @param second number
+     * @param third number
+     * @return Max number
+     */
+
+    public int max(int first, int second, int third) {
+        int result;
+
+        if (first > second && first > third) {
+            result = first;
+        } else if (second > first && second > third) {
+            result = second;
+
+        } else {
+            result = third;
+        }
+
+
+        return result;
+
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Comparison of three numbers
+ *
+ * @author Aliaksandr Kuzura (vorota-24@bk.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class MultiMaxTest {
+
+    /**
+     * first test
+     */
+
+    @Test
+    public void whenSecondMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    /**
+     * second test
+     */
+
+    @Test
+    public void whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(4, 1, 2);
+        assertThat(result, is(4));
+    }
+
+    /**
+     * third test
+     */
+
+    @Test
+    public void whenThirdMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(1, 2, 4);
+        assertThat(result, is(4));
+    }
+}
