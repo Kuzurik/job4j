269e9946bb4a7d32aced04340e651fce7052f9fe
kuzurik <vorota-24@bk.ru>
2019-06-16 17:47:11 +0300

    6.2. Перевернуть массив. [#143004]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * flip the array
+ * @author Aliaksandr Kuzura (vorota-24@bk.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+
+public class Turn {
+
+    /**
+     * flip the array
+     * @param array array for coup
+     * @return inverted array
+     */
+
+    public int[] back(int[] array) {
+       for (int index = 0; index != array.length / 2; index++) {
+           int tmp = array[index];
+           array[index] = array[array.length - index - 1];
+           array[array.length - index -1] = tmp;
+        }
+
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test flip the array
+ * @author Aliaksandr Kuzura (vorota-24@bk.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class TurnTest {
+
+    /**
+     * first test even array
+     */
+
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        Turn turner = new Turn();
+        int[] input = new int[] {4, 1, 6, 2};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {2, 6, 1, 4};
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * second test not even array
+     */
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        Turn turner = new Turn();
+        int[] input = new int[] {1, 2, 3, 4, 5};
+        int[] result = turner.back(input);
+        int[] expect = new int[] {5, 4, 3, 2, 1};
+        assertThat(result, is(expect));
+    }
+}
