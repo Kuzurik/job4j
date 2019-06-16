f4095bbbe6bab33b55151778c89196341aa4bb3e
kuzurik <vorota-24@bk.ru>
2019-06-16 18:29:31 +0300

    6.1. Классический поиск перебором. [#142995]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -21,6 +21,7 @@ public class FindLoop {
         for (int index = 0; index != data.length; index++) {
             if (data[index] == el) {
                 rst = index;
+                break;
             }
 
         }
