66459dc6ae3d45edecf5fcbdbd21ec54a0600f5e
Kuzurik <vorota-24@bk.ru>
2019-08-12 22:30:50 +0300

    5. Рефакторинг теста @Before @After[#143070]

--- chapter_002/src/test/java/ru/job4j/shape/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/shape/PaintTest.java
@@ -1,8 +1,11 @@
 package ru.job4j.shape;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,6 +14,20 @@ import static org.junit.Assert.assertThat;
  */
 
 public class PaintTest {
+	private final PrintStream stdout = System.out;
+	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+	
+	@Before
+	public void loadOutput() {
+		System.out.println("execute before method");
+		System.setOut(new PrintStream(this.out));
+	}
+	
+	@After
+	public void backOutput() {
+		System.setOut(this.stdout);
+		System.out.println("execute after method");
+	}
     
     /**
     *Test class Paint with class Square. 
@@ -18,9 +35,6 @@ public class PaintTest {
     
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 new String(out.toByteArray()),
@@ -33,7 +47,6 @@ public class PaintTest {
                             .toString()
                 )
         );
-        System.setOut(stdout);    
     }
     
     /**
@@ -42,9 +55,6 @@ public class PaintTest {
     
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(
                 new String(out.toByteArray()),
@@ -57,6 +67,5 @@ public class PaintTest {
                             .toString()
             )
         );
-        System.setOut(stdout);
     }
 }
