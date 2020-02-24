8b9c28144418345acea5f180536259370f664089
Kuzurik <vorota-24@bk.ru>
2020-02-24 21:35:37 +0300

    5.3.1. Создать динамический список на базе массива.[#237707]

---
+++ collectionPro/src/main/java/simpleArrayList/ListArray.java
@@ -0,0 +1,66 @@
+package simpleArrayList;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ListArray<T> implements Iterable{
+    private Object[] container;
+    private final static int DEFAULT_CAPACITY = 10;
+    private int position = 0;
+    private int modCount = 0;
+
+    public ListArray(){
+        this.container = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(T model) {
+        if (this.container.length == this.position) {
+            this.container = Arrays.copyOf(this.container, this.position * 2);
+        }
+        this.container[position++] = model;
+        modCount++;
+    }
+
+    public T get(int index) {
+        if (index > this.container.length) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) container[index];
+    }
+
+    public void remove(int index) {
+        if (index > this.container.length) {
+            throw new IndexOutOfBoundsException();
+        }
+        System.arraycopy(this.container, index + 1, this.container, index, this.container.length - 1 - index );
+        position--;
+        modCount++;
+    }
+
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            private int expectedModCount = modCount;
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                    if (expectedModCount != modCount) {
+                        throw new ConcurrentModificationException();
+                    }
+                return cursor < position;
+            }
+
+            @Override
+            public Object next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[cursor++];
+            }
+        };
+    }
+}
---
+++ collectionPro/src/test/java/simpleArrayList/ListArrayTest.java
@@ -0,0 +1,62 @@
+package simpleArrayList;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ListArrayTest {
+    private ListArray<Integer> list;
+
+    @Before
+    public void beforeTest() {
+        list = new ListArray<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void whenAddReturnFirstElement() {
+        assertThat(list.get(1), is(1));
+    }
+
+    @Test
+    public void whenAddThirtyElements() {
+        for(int i = 0; i != 30; i++) {
+            list.add(i);
+        }
+    }
+
+    @Test
+    public void whenRemoveReturnFirstElement() {
+        list.remove(0);
+        assertThat(list.get(0), is(1));
+    }
+
+    @Test
+    public void whenTestIterator() {
+        Iterator interList = list.iterator();
+        assertThat(interList.hasNext(), is(true));
+        assertThat(interList.next(), is(0));
+        assertThat(interList.next(), is(1));
+        assertThat(interList.next(), is(2));
+        assertThat(interList.next(), is(3));
+        assertThat(interList.hasNext(), is(false));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenNextRemoveValueReturnException() {
+        Iterator interList = list.iterator();
+        interList.next();
+        interList.next();
+        list.remove(1);
+        interList.next();
+    }
+}
