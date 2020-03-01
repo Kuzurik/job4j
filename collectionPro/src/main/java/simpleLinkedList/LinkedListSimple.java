a370e95657eaf8c5984df42c5cd99c41d972f737
Kuzurik <vorota-24@bk.ru>
2020-03-01 21:16:26 +0300

    5.3.2. Создать контейнер на базе связанного списка [#237708]

---
+++ collectionPro/src/main/java/simpleLinkedList/LinkedListSimple.java
@@ -0,0 +1,113 @@
+package simpleLinkedList;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedListSimple<E> implements Iterable<E> {
+    private int size = 0;
+    private Node<E> first;
+    private Node<E> last;
+    private int modCount = 0;
+
+    public void add(E data) {
+        if (this.size == 0) {
+            Node<E> newLink = new Node<>(data);
+            this.first = newLink;
+            this.last = newLink;
+        } else {
+            Node<E> newLink = new Node<>(data);
+            newLink.prev = this.last;
+            last.next = newLink;
+            this.last = newLink;
+        }
+        this.size++;
+        this.modCount++;
+    }
+
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.item;
+    }
+
+    E unlink(Node<E> x) {
+        // assert x != null;
+        final E element = x.item;
+        final Node<E> next = x.next;
+        final Node<E> prev = x.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size--;
+        modCount++;
+        return element;
+    }
+
+    Node<E> node(int index) {
+        Node<E> node = this.first;
+          for (int i = 0; i < index; i++) {
+               node = node.next;
+
+        }
+        return node;
+    }
+
+    public E remove(int index) {
+        return unlink(node(index));
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(E element) {
+            this.item = element;
+        }
+    }
+
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private Node<E> node = first;
+            private int expectedModCount = modCount;
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return cursor < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E result = node.item;
+                node = node.next;
+                cursor++;
+                return result;
+            }
+        };
+    }
+}
---
+++ collectionPro/src/test/java/simpleLinkedList/LinkedListSimpleTest.java
@@ -0,0 +1,54 @@
+package simpleLinkedList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class LinkedListSimpleTest {
+    private LinkedListSimple<Integer> list;
+    @Before
+    public void beforeTest() {
+        list = new LinkedListSimple<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void whenAddReturnFirstElement() {
+        assertThat(list.get(2), is(2));
+    }
+
+    @Test
+    public void whenRemoveReturnFirstElement() {
+        list.remove(0);
+        assertThat(list.get(0), is(1));
+    }
+
+    @Test
+    public void whenTestIterator() {
+        Iterator interList = list.iterator();
+        assertThat(interList.hasNext(), is(true));
+        assertThat(interList.next(), is(0));
+        assertThat(interList.next(), is(1));
+        assertThat(interList.next(), is(2));
+        assertThat(interList.next(), is(3));
+        assertThat(interList.hasNext(), is(false));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenNextRemoveValueReturnException() {
+        Iterator interList = list.iterator();
+        interList.next();
+        interList.next();
+        list.remove(1);
+        interList.next();
+    }
+}
