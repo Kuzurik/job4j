75f1a3230ab08864955e62430c16a5777db23a10
Kuzurik <vorota-24@bk.ru>
2020-03-03 19:25:01 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#237710]

---
+++ collectionPro/src/main/java/simpleLinkedList/ForwardLinked.java
@@ -0,0 +1,65 @@
+package simpleLinkedList;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public void revert() {
+        Node<T> node = head;
+        Node<T> prev = null;
+        while (node != null) {
+            Node<T> tmp = node.next;
+            node.next = prev;
+            prev = node;
+            head = node;
+            node = tmp;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
