0625c96b59e5e87acc9c326604ed6906e40daf72
Kuzurik <vorota-24@bk.ru>
2020-03-10 21:46:04 +0300

    1. Создать модель User[#237725]

---
+++ collectionPro/src/main/java/map/User.java
@@ -0,0 +1,13 @@
+package map;
+
+public class User {
+    private String name;
+    private int children;
+    private int birthday;
+
+    public User(String name, int children, int birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
