8728ff3a61abf7913673fac604a310bfa3eb380d
kuzurik <vorota-24@bk.ru>
2020-08-18 21:57:19 +0300

    1. Бот [#237754]

---
+++ io/src/main/java/ru/job4j/chatbot/ChatBot.java
@@ -0,0 +1,45 @@
+package ru.job4j.chatbot;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatBot {
+
+    public static void main(String[] args) throws Exception {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            boolean close = true;
+            while (close) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String str;
+                    String answer = "";
+                    String exit = "Exit";
+                    String hello = "Hello";
+                    while (!(str = in.readLine()).isEmpty()) {
+                        System.out.println(str);
+                        if (str.indexOf("msg=") > 0) {
+                            answer = str.split("msg=")[1];
+                            answer = answer.split("HTTP/1.1")[0];
+                            if (answer.contains(hello)) {
+                                answer = hello;
+                            } else {
+                                if (answer.contains(exit)) {
+                                    answer = exit;
+                                    close = false;
+                                }
+                            }
+                        }
+                    }
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(answer.getBytes());
+
+                }
+            }
+        }
+    }
+}
