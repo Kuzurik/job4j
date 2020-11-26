0e6e142ccfd1d8858110eabc274b241894813ab3
kuzurik <vorota-24@bk.ru>
2020-11-26 23:46:27 +0300

    1. Quartz [#237685]

---
+++ chapter_004/src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,56 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+       AlertRabbit cfg = new AlertRabbit();
+        int interval = cfg.readCfg("./chapter_004/src/main/resources/rabbit.properties");
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(interval)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public int readCfg(String path) {
+        int result = 0;
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                String[] parts = line.split("=");
+                result = Integer.parseInt(parts[1]);
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
