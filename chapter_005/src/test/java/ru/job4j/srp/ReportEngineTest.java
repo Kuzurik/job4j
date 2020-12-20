39ca88833b11c69cb2098148995efceae1a1f879
kuzurik <vorota-24@bk.ru>
2020-12-20 22:08:21 +0300

    Revert "1. Использую класса Calculator. Сделать класс InteractCalc. [#237761]"
    This reverts commit cf1e5e07f7398225d1e5d727e0fd7e5e8d526aa8.

--- chapter_005/src/test/java/ru/job4j/srp/ReportEngineTest.java
+++ chapter_005/src/test/java/ru/job4j/srp/ReportEngineTest.java
@@ -15,13 +15,6 @@ import static org.hamcrest.core.Is.is;
  */
 
 public class ReportEngineTest {
-
-    /**
-     * the variable stores the break to the next line.
-     */
-
-    private final String ln = System.lineSeparator();
-
     /**
      * this method tests first method of report.
      */
@@ -34,12 +27,12 @@ public class ReportEngineTest {
         ReportEngine engine = new ReportEngine(store);
         StringBuilder expect = new StringBuilder()
                 .append("Name; Hired; Fired; Salary;")
-                .append(ln)
+                .append(System.lineSeparator())
                 .append(worker.getName()).append(";")
                 .append(worker.getHired()).append(";")
                 .append(worker.getFired()).append(";")
                 .append(worker.getSalary()).append(";")
-                .append(ln);
+                .append(System.lineSeparator());
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 
@@ -50,6 +43,7 @@ public class ReportEngineTest {
     @Test
     public void whenHtmlReport() {
         ChangeFormat date = new DateFormat();
+        String lS = System.lineSeparator();
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
         Employee worker = new Employee("Ivan", now, now, 100);
@@ -60,30 +54,30 @@ public class ReportEngineTest {
         store.add(worker2);
         DeveloperReport report = new DeveloperReport(store,date);
         StringBuilder expect = new StringBuilder();
-        expect.append("<!DOCTYPE html>").append(ln)
-                .append("<html>").append(ln)
-                .append("<head>").append(ln)
-                .append("<meta charset=\"utf-8\"/>").append(ln)
-                .append("<title>Report employee</title>").append(ln)
-                .append("</head>").append(ln)
-                .append("<body>").append(ln)
-                .append("<table border='1'; cellpadding='10'; style='width:100%; border-radius:5px;'>").append(ln)
-                .append("<tr>").append(ln)
+        expect.append("<!DOCTYPE html>").append(lS)
+                .append("<html>").append(lS)
+                .append("<head>").append(lS)
+                .append("<meta charset=\"utf-8\"/>").append(lS)
+                .append("<title>Report employee</title>").append(lS)
+                .append("</head>").append(lS)
+                .append("<body>").append(lS)
+                .append("<table border='1'; cellpadding='10'; style='width:100%; border-radius:5px;'>").append(lS)
+                .append("<tr>").append(lS)
                 .append("<th>Name</th>")
                 .append("<th>Hired</th>")
                 .append("<th>Fired</th>")
                 .append("<th>Salary</th>")
-                .append("</th>").append(ln);
+                .append("</th>").append(lS);
         for(Employee employee : store.findBy(em -> true)) {
-            expect.append("<tr>").append(ln)
+            expect.append("<tr>").append(lS)
                     .append(String.format("<td>%s</td>", employee.getName()))
                     .append(String.format("<td>%s</td>",date.date(employee.getHired())))
                     .append(String.format("<td>%s</td>", date.date(employee.getFired())))
                     .append(String.format("<td>%s</td>", employee.getSalary()))
-                    .append("</tr>").append(ln);
+                    .append("</tr>").append(lS);
         }
-        expect.append("</table>").append(ln)
-                .append("</body>").append(ln)
+        expect.append("</table>").append(lS)
+                .append("</body>").append(lS)
                 .append("</html>");
         assertThat(report.generate(em -> true), is(expect.toString()));
     }
@@ -102,12 +96,12 @@ public class ReportEngineTest {
         AccountingDepartmentReport engine = new AccountingDepartmentReport(store,convert);
         StringBuilder expect = new StringBuilder()
                 .append("Name; Hired; Fired; Salary;")
-                .append(ln)
+                .append(System.lineSeparator())
                 .append(worker.getName()).append(";")
                 .append(worker.getHired()).append(";")
                 .append(worker.getFired()).append(";")
                 .append("1.0$").append(";")
-                .append(ln);
+                .append(System.lineSeparator());
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 
@@ -127,57 +121,13 @@ public class ReportEngineTest {
         store.add(worker2);
         HRDepartmentReport engine = new HRDepartmentReport(store);
         StringBuilder expect = new StringBuilder()
-                .append("Name; Salary;").append(ln)
+                .append("Name; Salary;").append(System.lineSeparator())
                 .append(worker2.getName()).append(";")
-                .append(worker2.getSalary()).append(";").append(ln);
+                .append(worker2.getSalary()).append(";").append(System.lineSeparator());
         expect.append(worker1.getName()).append(";")
-                .append(worker1.getSalary()).append(";").append(ln);
+                .append(worker1.getSalary()).append(";").append(System.lineSeparator());
         expect.append(worker.getName()).append(";")
-                .append(worker.getSalary()).append(";").append(ln);
+                .append(worker.getSalary()).append(";").append(System.lineSeparator());
         assertThat(engine.generate(em -> true), is(expect.toString()));
-    }
-
-    /**
-     * this method test a report in json format.
-     */
-
-
-    @Test
-    public void whenGeneratedJSONReport() {
-        MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
-        Employee worker = new Employee("Ivan", now, now, 100);
-        store.add(worker);
-        Report jsonReport = new JsonReport(store);
-        StringBuilder expect = new StringBuilder()
-                .append("{").append(ln)
-                .append("\"Name\" : \"").append(worker.getName()).append("\",").append(ln)
-                .append("\"Hired\" : \"").append(worker.getHired().getTime()).append("\",").append(ln)
-                .append("\"Fired\" : \"").append(worker.getFired().getTime()).append("\",").append(ln)
-                .append("\"Salary\" : \"").append(worker.getSalary()).append("\"").append(ln)
-                .append("}");
-        assertThat(jsonReport.generate(employee -> true), is(expect.toString()));
-    }
-
-    /**
-     * this method test a report in xml format.
-     */
-
-    @Test
-    public void whenGeneratedXMLReport() {
-        MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
-        Employee worker = new Employee("Ivan", now, now, 100);
-        store.add(worker);
-        Report xmlReport = new XmlReport(store);
-        StringBuilder expect = new StringBuilder()
-                .append("?<xml version=\"1.0\" encoding=\"UNF-8\"?>").append(ln)
-                .append("<employee>").append(ln)
-                .append("<name>").append(worker.getName()).append("</name>").append(ln)
-                .append("<hired>").append(worker.getHired().getTime()).append("</hired>").append(ln)
-                .append("<fired>").append(worker.getFired().getTime()).append("</fired>").append(ln)
-                .append("<salary>").append(worker.getSalary()).append("</salary>").append(ln)
-                .append("</employee>");
-        assertThat(xmlReport.generate(employee -> true), is(expect.toString()));
     }
 }
